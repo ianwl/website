6a175ccd17a02484bbc2c70878c4a535d05d823f
ianwl <ian@wanlian.tw>
2020-05-21 11:56:34 +0800

    add ci

--- .circleci/push_ftp.sh
+++ .circleci/push_ftp.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if [ "$CIRCLE_BRANCH" = "release" ]; then
-      git ftp push -u "$BT_FTP_ACCOUNT_RELEASE" -p "$BT_FTP_PASSWORD_RELEASE" ftp://"$BT_URL_RELEASE"
+    git ftp init -u "$BT_FTP_ACCOUNT_RELEASE" -p "$BT_FTP_PASSWORD_RELEASE" ftp://"$BT_URL_RELEASE"
     echo "is release"
+
+elif [ "$CIRCLE_BRANCH" = "ci-test" ]; then
+    git ftp init -u "$BT_FTP_ACCOUNT_RELEASE" -p "$BT_FTP_PASSWORD_RELEASE" ftp://"$BT_URL_RELEASE"
+    echo "is ci-test"
 
 fi
