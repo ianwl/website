424d638d1dd221a570405e260dbf90f06faa59b1
ianwl <ian@wanlian.tw>
2020-05-22 14:35:44 +0800

    update circleci config

--- .circleci/push_ftp.sh
+++ .circleci/push_ftp.sh
@@ -5,7 +5,7 @@ if [ "$CIRCLE_BRANCH" = "release/prod" ]; then
     echo "is release"
 
 elif [ "$CIRCLE_BRANCH" = "release/dev" ]; then
-    git ftp push -u "$BT_FTP_ACCOUNT_RELEASE" -p "$BT_FTP_PASSWORD_RELEASE" ftp://"$BT_URL_RELEASE"
+    git ftp push -u "$BT_FTP_ACCOUNT_DEV" -p "$BT_FTP_PASSWORD_DEV" ftp://"$BT_URL_DEV"
     echo "is dev"
 
 fi
